7702f528cd2b8c85106391d10389d4974797181d
jahidhasan8 <jahidhasan1932@gmail.com>
2022-11-17 16:14:23 +0600

    jwt token added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
 const port = process.env.PORT || 5000
@@ -31,6 +31,7 @@ async function run() {
     try {
         const appointmentOptionCollection = client.db('doctors-portal').collection('appointmentOptions')
         const bookingCollection = client.db('doctors-portal').collection('bookings')
+        const usersCollection = client.db('doctors-portal').collection('users')
 
         //  use aggregate to query multiple collection and then merge data
         app.get('/appointmentOptions', async (req, res) => {
@@ -99,6 +100,15 @@ async function run() {
             res.send(options)
         })
 
+          
+         app.get('/bookings',async(req,res)=>{
+            const email=req.query.email 
+            const query={
+                email:email
+            }
+            const bookings=await bookingCollection.find(query).toArray()
+            res.send(bookings)
+         })
 
         app.post('/bookings', async (req, res) => {
             const booking = req.body
@@ -115,6 +125,23 @@ async function run() {
                  return res.send({acknowledged:false,message})
             }
             const result = await bookingCollection.insertOne(booking)
+            res.send(result)
+        });
+         
+        app.get('/jwt',async(req,res)=>{
+            const email=req.query.email
+            const query={email:email}
+            const user=await usersCollection.findOne(query)
+            if(user){
+                const token=jwt.sign({email},process.env.ACCESS_TOKEN,{expiresIn:'1d'})
+                return res.send({accessToken:token})
+            }
+            res.status(403).send({accessToken:''})
+
+        })
+        app.post('/users',async(req,res)=>{
+            const user=req.body
+            const result=await usersCollection.insertOne(user)
             res.send(result)
         })
     }
